93212fe69224064f22fd59f698d5c9bd7f89aa8b
Kanwal25Dev <kamal.dhera360@gmail.com>
2026-01-03 06:05:47 +0500

    getuserbyid & updateuser & delete Updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -94,5 +94,91 @@ module.exports = router
 
 
 //----------/api/user/getuserbyid
+//get
+router.get('/:id', async (req, res) => {
+  try {
+    let { id } = req.params
+    let user = await User.findById(id)
+
+    if (user) {
+      res.json({
+        message: "user get successfuly",
+        user
+      })
+    } else {
+      res.json({
+        message: "User not found"
+      })
+    }
+  } catch (error) {
+    res.json({
+      message: error.message
+    })
+  }
+})
+
+
 //----------/api/user/updateuser
+//put
+router.put('/:id', async (req, res) => {
+  try {
+    let { id } = req.params
+    let { name, email, password, role, age } = req.body
+
+    let user = await User.findById(id)
+    if (!user) {
+      return res.json({ message: "User not found" })
+    }
+
+    // if password comes, hash it
+    if (password) {
+      password = await bcrypt.hash(password, 10)
+    }
+
+    let updateduser = await User.findByIdAndUpdate(
+      id,
+      {
+        name: name || user.name,
+        email: email || user.email,
+        password: password || user.password,
+        role: role || user.role,
+        age: age || user.age
+      },
+      { new: true }
+    )
+
+    res.json({
+      message: "user update successfuly",
+      updateduser
+    })
+  } catch (error) {
+    res.json({
+      message: error.message
+    })
+  }
+})
+
+
 //----------/api/user/delete
+//delete
+router.delete('/:id', async (req, res) => {
+  try {
+    let { id } = req.params
+    let deleteduser = await User.findByIdAndDelete(id)
+
+    if (deleteduser) {
+      res.json({
+        message: "user delete successfuly",
+        deleteduser
+      })
+    } else {
+      res.json({
+        message: "User not found"
+      })
+    }
+  } catch (error) {
+    res.json({
+      message: error.message
+    })
+  }
+})
